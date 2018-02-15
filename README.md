eebba16766fabfbf5be6b063e7fe38c4476337eb
Muhamed Handukic <muhamedhandukic@gmail.com>
2018-02-15 22:04:25 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# All-NBA 1st team classifier
+
+The All-NBA Team is an annual National Basketball Association (NBA) honor bestowed on the best players in the league following every NBA season. This is a simple machine learning approach on classifying players based on their stats. 
+
+## Player stats
+- FG % (Field goal percentage)
+- PPG (Points per game)
+- RPG (Rebounds per game)
+- APG (Assists per game)
+- PER (Player efficiency rating)
+
+## Classifier accuracy
+- 70 % on test set (not bad, but could be better)
+
+## 2018 stats
+I wanted to see how the classifier does on stats from some players from the 2017/18 season:
+
+![Stats](img/players.png)
+
+Results:
+All-NBA 1st team: Lebron James, James Harden
+Didn't make it: Paul George, Kemba Walker
+
+James Harden when he realized he's gonna make the All-Nba 1st team:
+![Harden](img/harden.jpg)
+
+## ToDo List
+- Add more data
+- Add more stat attributes (current attributes can't describe how good a player really is)
+- Try different model
---
+++ allNbaClassifier.py
@@ -0,0 +1,36 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+
+dataset = pd.read_csv('allNba.csv')
+x = dataset.iloc[:,2:7].values
+y = dataset.iloc[:,-1].values
+
+#Splitting into training set and test set
+X_train, X_test, Y_train, Y_test = train_test_split(x, y, test_size = 0.2, random_state = 0)
+
+#Feature scaling
+scaler =  StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.fit_transform(X_test)
+
+#Logistic regression model
+nbaClassifier =  LogisticRegression(random_state = 0)
+nbaClassifier.fit(X_train,Y_train)
+
+#Prediction based on test set
+Y_pred = nbaClassifier.predict(X_test)
+
+#Classifier accuracy
+accuracy = accuracy_score(Y_test,Y_pred)
+
+#Testing classifier on stats from current season
+currentSeasonData = pd.read_csv('allNba2018.csv')
+currentSeasonTest = currentSeasonData.iloc[:,2:7].values
+currentSeasonTest = scaler.fit_transform(currentSeasonTest)
+
+#Prediction based on 2018 player stats
+currentSeasonPrediction = nbaClassifier.predict(currentSeasonTest)
+
